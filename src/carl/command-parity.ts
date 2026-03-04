dab6792747f9aa51964991be06d000db5678c5cf
Kris Gray <kag56@cam.ac.uk>
2026-03-04 19:54:56 +0000

    fix(09-01): correct ES module imports for fs/path/os
    - Change default imports to namespace imports (* as fs)
    - Add esModuleInterop and allowSyntheticDefaultImports to tsconfig
    - Fixes TypeScript compilation errors preventing tests from running

--- src/carl/command-parity.ts
+++ src/carl/command-parity.ts
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+import * as fs from "fs";
+import * as path from "path";
 import type { CarlRuleDomainPayload } from "./types";
 import { buildCarlDocsGuidance } from "./help-text";
 
--- src/carl/setup.ts
+++ src/carl/setup.ts
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+import * as fs from "fs";
+import * as path from "path";
 import { findProjectCarl, findGlobalCarl } from "../integration/paths";
 import {
   integrateCarl,
